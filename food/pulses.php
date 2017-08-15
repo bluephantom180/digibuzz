d0a59029eb313aaedb73587a13f4ac772a2e48c7
bluephantom180 <sriramakrishna13@gmail.com>
2017-08-15 19:12:14 +0530

    klaevw

---
+++ food/pulses.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Organo</title>
+<link href='https://fonts.googleapis.com/css?family=Lobster+Two:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,300,700' rel='stylesheet' type='text/css' />
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+<!--MOBILE DEVICE-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+<!--CSS-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css"/>
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+<link rel="stylesheet" type="text/css" href="css/menu.css"/>
+
+<!--JS-->
+
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/modernizr.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/waypoints.min.js"></script>
+</head>
+
+<body>
+<!-- Paste this code after body tag -->
+<div class="se-pre-con"></div>
+<!-- Ends -->
+
+<!-- Header Section -->
+
+<header>
+  <div class="container">
+    <div class="row clearfix" id="home">
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="logo">
+          <h1> <a href="#"><img src="images/5.png" alt="" width="180px" height="80px"/></a> </h1>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <div class="contact">
+          <p>Call us Toll-free!<span class="number"><a href="#">1800-0000-7777</a></span><span class="time">(9AM to 7PM)</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<!-- End Header Section -->
+
+<!-- Items Menu Section -->
+
+<section class="saction4">
+	<div class="container">
+	<div class="dropdown">
+  <button class="dropbtn active">Rice</button>
+  <div class="dropdown-content">
+    <a href="#">Basmathi</a>
+    <a href="#">Fully Polished</a>
+    <a href="#">Semi Polished</a>
+    <a href="#">Unpolished</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Vegetables</button>
+  <div class="dropdown-content">
+    <a href="#">Potato</a>
+    <a href="#">Oninon</a>
+    <a href="#">Tomato</a>
+    <a href="#">Carrot</a>
+    <a href="#">Raddish</a>
+    <a href="#">Beetroot</a>
+    <a href="#">...</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Pulses</button>
+  <div class="dropdown-content">
+    <a href="#">Green Gram</a>
+    <a href="#">Green Peas</a>
+    <a href="#">Penuts</a>
+    <a href="#">Dhal</a>
+    <a href="#">Urad Dhal</a>
+    <a href="#">Moong Dhal</a>
+    <a href="#">Lobia</a>
+    <a href="#">...</a>
+  </div>
+</div>
+
+<div class="dropdown">
+  <button class="dropbtn">Milk Products</button>
+  <div class="dropdown-content">
+    <a href="#">Milk</a>
+    <a href="#">Ghee</a>
+	<a href="#">Butter</a>
+    <a href="#">Cheese</a>
+  </div>
+</div>
+
+</div>
+<!-- End Items Menu Section -->
+<br>
+<!-- Types -->
+
+<div class="container">
+	<div class="brdr clearfix">
+		<img class="img2" src="images/bs.png" alt="" width="150px" height="175px"><br>
+		<h5><span style="padding-top:50px;padding-left:15px;">Basmathi Rice</h5></span>
+		<h6><span style="padding-left:15px; color:#bfbfbf;vertical-align:middle"> Long and beautiful </h6></span>
+		<button> - <input class="tb" type="text" name="quantity"> + </button>
+	</div>
+	<span style="padding-left:50px"></span>
+	<div class="brdr clearfix">
+		<img class="img2" src="images/bs.png" alt="" width="150" height="175px"><br>
+		<h5><span style="padding-left:15px;">Polished Rice</h5></span>
+		<h6><span style="padding-left:15px; color:#bfbfbf;">Beautiful White Rice</h6></span>
+		
+	</div><br><br>
+	<div class="brdr clearfix">
+		<img class="img2" src="images/bs.png" alt="" width="150" height="175px"><br>
+		<h5><span style="padding-left:15px;">Semi-Polished Rice</h5> </span>
+		<h6><span style="padding-left:15px; color:#bfbfbf;">Perfect for people undergoing dieting</h6></span>
+	</div>
+		<span style="padding-left:50px"></span>
+	<div class="brdr clearfix">
+		<img class="img2" src="images/bs.png" alt="" width="150" height="175px"><br>
+		<h5><span style="padding-left:15px;">Unpolished Rice</h5> </span>
+		<h6><span style="padding-left:15px;color:#bfbfbf;">Unpolished Brown Rice</h6></span>
+	</div>
+	
+</div>
+
+<!-- End Types -->
+</section>
+<!-- Footer Section -->
+
+<footer class="saction8">
+  <div class="container" id="contact">
+    <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="site">
+          <h3>Organo</h3>
+        </div>
+        <div class="sitelink">
+          <ul>
+            <li> <span>&#10152;</span><a href="#">About Us</a></li>
+            <li><span>&#10152;</span><a href="#">Privacy Policy</a></li>
+            <li><span>&#10152;</span><a href="#">Terms of Use</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="site">
+          <h3>Site Link</h3>
+        </div>
+        <div class="sitelink">
+          <ul>
+            <li><span>&#10152;</span><a href="#">Contact Us</a></li>
+            <li><span>&#10152;</span><a href="#">FAQ</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3">
+        <div class="follow">
+          <h3>Follow Us On...</h3>
+        </div>
+        <div class="social">
+          <ul>
+            <li> <i class="fa fa-facebook-square"></i><a href="#">Facebook</a></li>
+            <li><i class="fa fa-twitter-square"></i><a href="#">Twitter</a></li>
+            <li><i class="fa fa-linkedin-square"></i><a href="#">Linkedin</a></li>
+            <li><i class="fa fa-google-plus-square"></i><a href="#">Google Plus</a></li>
+          </ul>
+        </div>
+      </div>
+</footer>
+
+<!-- End Footer Section -->
+
+<!-- 2nd Footer Section -->
+
+<footer class="saction9">
+  <div class="container">
+        <div class="email">&copy; All right reserved 2017  /  Organo </div>
+    </div>
+  </div>
+</footer>
+
+<!-- End 2nd Footer Section -->
+
+<script type="text/javascript" src="js/sidemenu.js"></script>
+</body>
+</html>
